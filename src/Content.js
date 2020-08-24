925e75d08241a828842b3713055d61424ecef3c9
Muhammad Naufal <mnaufal75@gmail.com>
2020-08-24 08:40:01 +0700

    convert all components to functional component

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Content extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Content = (props) => {
+  const { title, firstName, lastName, yourCity, name, company, role, phoneNumber } = props.data;
 
-  render() {
-    const { title, firstName, lastName, yourCity, name, company, role, phoneNumber } = this.props.data;
-
-    if (title === 'Card A') {
-      return (
-        <div className="content">
-          <h3>{title}</h3>
-          <h4>First Name: {firstName}</h4>
-          <h4>Last Name: {lastName}</h4>
-          <h4>Your City: {yourCity}</h4>
-        </div>
-      );
-    } else if (title === 'Card B') {
-      return (
-        <div className="content">
-          <h3>{title}</h3>
-          <h4>Name: {name}</h4>
-          <h4>Company: {company}</h4>
-          <h4>Role: {role}</h4>
-          <h4>Phone Number: {phoneNumber}</h4>
-        </div>
-      );
-    }
+  if (title === 'Card A') {
+    return (
+      <div className="content">
+        <h3>{title}</h3>
+        <h4>First Name: {firstName}</h4>
+        <h4>Last Name: {lastName}</h4>
+        <h4>Your City: {yourCity}</h4>
+      </div>
+    );
+  } else if (title === 'Card B') {
+    return (
+      <div className="content">
+        <h3>{title}</h3>
+        <h4>Name: {name}</h4>
+        <h4>Company: {company}</h4>
+        <h4>Role: {role}</h4>
+        <h4>Phone Number: {phoneNumber}</h4>
+      </div>
+    );
   }
 }
 
--- src/LeftMenu.js
+++ src/LeftMenu.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './LeftMenu.css';
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-
-    this.sendData = this.sendData.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
-  }
-
-  sendData(key) {
+const Menu = (props) => {
+  const sendData = (key) => {
     console.log(key);
-    return this.props.parentCallback(key);
+    return props.parentCallback(key);
   }
 
-  closeMenu() {
-    return this.props.parentCallback2();
+  const closeMenu = () => {
+    return props.parentCallback2();
   }
 
-  render() {
-    const data = this.props.data;
-    const listCard = Object.keys(data).map((key) =>
-      <li key={key} onClick={() => this.sendData(key)} >{data[key].title}</li>
-    );
+  const data = props.data;
+  const listCard = Object.keys(data).map((key) =>
+    <li key={key} onClick={() => sendData(key)} >{data[key].title}</li>
+  );
 
-    return (
-      <div className="leftMenu">
-        <ul>{listCard}</ul>
+  return (
+    <div className="leftMenu">
+      <ul>{listCard}</ul>
 
-        <img alt="closeButton" className="closeButton" src="close" onClick={this.closeMenu} />
-      </div>
-    );
-  }
+      <img alt="closeButton" className="closeButton" src="close" onClick={closeMenu} />
+    </div>
+  );
 }
 
 export default Menu;
